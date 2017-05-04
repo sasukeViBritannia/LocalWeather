cda9f4f707c6be2ef633f84cca47563b874e76e5
Ale <ale@aleLinux>
2017-05-04 21:22:58 +0200

    Eliminate ora alba e tramonto perchè baggate

--- interrogazioni.js
+++ interrogazioni.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
     var lat = 45.30; /*latitudine di Lodi*/
     var lon = 9.50; /*longitudine di lodi*/
 
+
     /* if ("geolocation" in navigator) {
          navigator.geolocation.getCurrentPosition(function(position) {
              lat = position.coords.latitude;
@@ -23,6 +24,7 @@ $(document).ready(function() {
             })
             .done(function(data) {
                 window.alert('success');
+                var temp = '3h';
                 var risposta = $.parseJSON(data);
                 $('.cella').eq(0).find('span').text(risposta.coord.lat + ', ' + risposta.coord.lon);
                 $('.cella').eq(1).find('span').text(risposta.weather[0].main);
@@ -30,10 +32,8 @@ $(document).ready(function() {
                 $('.cella').eq(3).find('span').text(risposta.main.temp_min + ' - ' + risposta.main.temp_max);
                 $('.cella').eq(4).find('span').text(risposta.wind.speed);
                 $('.cella').eq(5).find('span').text(risposta.main.pressure);
-                $('.cella').eq(6).find('span').text(risposta.sys.sunrise);
-                $('.cella').eq(7).find('span').text(risposta.sys.sunset);
                 $('.cella').eq(8).find('span').text(risposta.main.humidity);
-                $('.cella').eq(9).find('span').text(risposta.rain.3h);
+                $('.cella').eq(9).find('span').text(risposta.rain.temp);
             })
             .fail(function() {
                 window.alert('error');
