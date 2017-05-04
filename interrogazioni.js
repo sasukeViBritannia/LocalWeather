22d6388167dd73662878462d65bb42e0cd2c9822
ale <ale@ubuntu>
2017-05-04 18:05:05 +0200

    Completata conversione JSON problema voce rain

--- interrogazioni.js
+++ interrogazioni.js
@@ -1,35 +1,46 @@
 'use strict';
 $(document).ready(function() {
 
-    var lat = 0; /*latitudine di Lodi*/
-    var lon = 0; /*longitudine di lodi*/
+    var lat = 45.30; /*latitudine di Lodi*/
+    var lon = 9.50; /*longitudine di lodi*/
 
-    if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            lat = Math.round(position.coords.latitude);
-            lon = Math.round(position.coords.longitude);
-            window.alert('Coordinate di Lodi: ' + lat + ', ' + lon);
-        });
-    } else
-        window.alert('Non è geolocalizzato');
+    /* if ("geolocation" in navigator) {
+         navigator.geolocation.getCurrentPosition(function(position) {
+             lat = position.coords.latitude;
+             lon = position.coords.longitude;
+             window.alert('Coordinate di Lodi: ' + lat + ', ' + lon);
+         });
+     } else
+         window.alert('Non è geolocalizzato');*/
 
 
-     $('#btn').on('click', function() {
+    $('#btn').on('click', function() {
 
-         $.ajax({
-                 url: 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&APPID=a9fa46b00d88fcc6181dac9c6db790e6',
-                 type: 'GET',
-                 dataType: 'json',
-             })
-             .done(function() {
-                 window.alert('success');
-             })
-             .fail(function() {
-                 window.alert('error');
-             })
-             .always(function(){
-             	window.alert('conclusa');
-             });
-     });
+        $.ajax({
+                url: 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&APPID=a9fa46b00d88fcc6181dac9c6db790e6',
+                type: 'GET',
+                dataType: 'text',
+            })
+            .done(function(data) {
+                window.alert('success');
+                var risposta = $.parseJSON(data);
+                $('.cella').eq(0).find('span').text(risposta.coord.lat + ', ' + risposta.coord.lon);
+                $('.cella').eq(1).find('span').text(risposta.weather[0].main);
+                $('.cella').eq(2).find('span').text(risposta.main.temp);
+                $('.cella').eq(3).find('span').text(risposta.main.temp_min + ' - ' + risposta.main.temp_max);
+                $('.cella').eq(4).find('span').text(risposta.wind.speed);
+                $('.cella').eq(5).find('span').text(risposta.main.pressure);
+                $('.cella').eq(6).find('span').text(risposta.sys.sunrise);
+                $('.cella').eq(7).find('span').text(risposta.sys.sunset);
+                $('.cella').eq(8).find('span').text(risposta.main.humidity);
+                $('.cella').eq(9).find('span').text(risposta.rain.3h);
+            })
+            .fail(function() {
+                window.alert('error');
+            })
+            .always(function() {
+                window.alert('conclusa');
+            });
+    });
 
 });
