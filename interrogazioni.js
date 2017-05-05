cab13b543aae445bbb5b9021bb84bf96ad30f693
ale <ale@ubuntu>
2017-05-05 17:04:01 +0200

    Eliminate le previsioni

--- interrogazioni.js
+++ interrogazioni.js
@@ -21,19 +21,22 @@ $(document).ready(function() {
     $('#btn').on('click', function() {
 
         $.ajax({
-                url: 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&APPID=a9fa46b00d88fcc6181dac9c6db790e6&units=metric',
+                url: 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&APPID=a9fa46b00d88fcc6181dac9c6db790e6&units=metric',
                 type: 'GET',
                 dataType: 'json',
             })
             .done(function(risposta) {
                 window.alert('success');
-                $('.cella').eq(0).find('span').text(risposta.city.coord.lat + ', ' + risposta.city.coord.lon);
-                $('.cella').eq(1).find('span').text(risposta.list[0].weather[0].main);
-                $('.cella').eq(2).find('span').text(Math.round(risposta.list[0].main.temp));
-                $('.cella').eq(3).find('span').text(risposta.list[0].wind.speed);
-                $('.cella').eq(4).find('span').text(risposta.list[0].main.pressure);
-                $('.cella').eq(5).find('span').text(risposta.list[0].main.humidity);
-                $('.cella').eq(6).find('span').text(risposta.list[0].rain["3h"]);
+                $('.cella').eq(0).find('span').text(risposta.coord.lat + ', ' + risposta.coord.lon);
+                $('.cella').eq(1).find('span').text((new Date(risposta.dt * 1000)).toLocaleDateString());
+                $('.cella').eq(2).find('span').text(risposta.weather[0].main);
+                $('.cella').eq(3).find('span').text(Math.round(risposta.main.temp));
+                $('.cella').eq(4).find('span').text(risposta.wind.speed);
+                $('.cella').eq(5).find('span').text(risposta.main.pressure);
+                $('.cella').eq(6).find('span').text(risposta.main.humidity);
+                $('.cella').eq(7).find('span').text((new Date(risposta.sys.sunrise * 1000)).toLocaleTimeString().slice(0, 5));
+                $('.cella').eq(8).find('span').text((new Date(risposta.sys.sunset * 1000)).toLocaleTimeString().slice(0, 5));
+                $('.flexCol').find('img').attr('src', 'http://openweathermap.org/img/w/'+risposta.weather.icon+'.png');
             })
             .fail(function() {
                 window.alert('error');
