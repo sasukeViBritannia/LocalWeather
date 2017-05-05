72f27df3d405a19d8cb532dcb09f8883d2326f12
ale <ale@ubuntu>
2017-05-05 12:51:44 +0200

    Cambiate le api da current a forecast ed aggiunte in html le previsioni in più

--- interrogazioni.js
+++ interrogazioni.js
@@ -4,6 +4,9 @@ $(document).ready(function() {
     var lat = 45.30; /*latitudine di Lodi*/
     var lon = 9.50; /*longitudine di lodi*/
 
+    /*lat = 43.7;
+    lon = -79.42;*/
+
 
     /* if ("geolocation" in navigator) {
          navigator.geolocation.getCurrentPosition(function(position) {
@@ -18,22 +21,19 @@ $(document).ready(function() {
     $('#btn').on('click', function() {
 
         $.ajax({
-                url: 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&APPID=a9fa46b00d88fcc6181dac9c6db790e6&units=metric',
+                url: 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&APPID=a9fa46b00d88fcc6181dac9c6db790e6&units=metric',
                 type: 'GET',
-                dataType: 'text',
+                dataType: 'json',
             })
-            .done(function(data) {
+            .done(function(risposta) {
                 window.alert('success');
-                window.alert(data);
-                var risposta = $.parseJSON(data);
-                $('.cella').eq(0).find('span').text(risposta.coord.lat + ', ' + risposta.coord.lon);
-                $('.cella').eq(1).find('span').text(risposta.weather[0].main);
-                $('.cella').eq(2).find('span').text(Math.round(risposta.main.temp));
-                $('.cella').eq(3).find('span').text(risposta.wind.speed);
-                $('.cella').eq(4).find('span').text(risposta.main.pressure);
-                $('.cella').eq(5).find('span').text(risposta.main.humidity);
-                $('.cella').eq(6).find('span').text((new Date(risposta.sys.sunrise *1000).toTimeString()).substring(0, 5));
-                $('.cella').eq(7).find('span').text((new Date(risposta.sys.sunset *1000).toTimeString()).substring(0, 5));
+                $('.cella').eq(0).find('span').text(risposta.city.coord.lat + ', ' + risposta.city.coord.lon);
+                $('.cella').eq(1).find('span').text(risposta.list[0].weather[0].main);
+                $('.cella').eq(2).find('span').text(Math.round(risposta.list[0].main.temp));
+                $('.cella').eq(3).find('span').text(risposta.list[0].wind.speed);
+                $('.cella').eq(4).find('span').text(risposta.list[0].main.pressure);
+                $('.cella').eq(5).find('span').text(risposta.list[0].main.humidity);
+                $('.cella').eq(6).find('span').text(risposta.list[0].rain["3h"]);
             })
             .fail(function() {
                 window.alert('error');
