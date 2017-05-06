59ca572ecf61fbd0511e7662a11031718e9bd4ad
ale <ale@ubuntu>
2017-05-06 17:41:52 +0200

    Rifinita la struttura principale e accenno di styling

--- interrogazioni.js
+++ interrogazioni.js
@@ -4,23 +4,23 @@ $(document).ready(function() {
     var lat = 45.30; /*latitudine di Lodi*/
     var lon = 9.50; /*longitudine di lodi*/
 
-    lat = 63.4667;
-    lon = 142.8167;
-    aziona(lat, lon);
+    /*lat = 63.4667;
+    lon = 142.8167;*/
+    /*aziona(lat, lon);*/
 
-    /*if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            lat = position.coords.latitude;
-            lon = position.coords.longitude;
-            window.alert('Coordinate di Lodi: ' + lat + ', ' + lon);
-            aziona(lat, lon);
-        });
-    } else
-        window.alert('Non è geolocalizzato');*/
+    /* if ("geolocation" in navigator) {
+         navigator.geolocation.getCurrentPosition(function(position) {
+             lat = position.coords.latitude;
+             lon = position.coords.longitude;
+             window.alert('Coordinate di Lodi: ' + lat + ', ' + lon);
+             /*aziona(lat, lon);
+         });
+     } else
+         window.alert('Non è geolocalizzato');*/
 
 
-    /* $('#btn').on('click', function() {*/
-    function aziona(lat, lon) {
+    $('#btn').on('click', function() {
+        /*function aziona(lat, lon) {*/
         $.ajax({
                 url: 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&APPID=a9fa46b00d88fcc6181dac9c6db790e6&units=metric',
                 type: 'GET',
@@ -28,16 +28,15 @@ $(document).ready(function() {
             })
             .done(function(risposta) {
                 window.alert('success');
-                $('.cella').eq(0).find('span').text(risposta.coord.lat + ', ' + risposta.coord.lon);
-                $('.cella').eq(1).find('span').text((new Date(risposta.dt * 1000)).toLocaleDateString());
-                $('.cella').eq(2).find('span').text(risposta.weather[0].main);
-                $('.cella').eq(3).children('#temp').text(Math.round(risposta.main.temp));
+                $('main').children('p').text('(' + risposta.coord.lat + ', ' + risposta.coord.lon + ')');
+                $('.cella').eq(0).find('span').text((new Date(risposta.dt * 1000)).toLocaleDateString());
+                $('.cella').eq(1).find('span').text(risposta.weather[0].main);
                 $('#temp').text(Math.round(risposta.main.temp));
-                $('.cella').eq(4).find('span').text(risposta.wind.speed);
-                $('.cella').eq(5).find('span').text(risposta.main.pressure);
-                $('.cella').eq(6).find('span').text(risposta.main.humidity);
-                $('.cella').eq(7).find('span').text((new Date(risposta.sys.sunrise * 1000)).toLocaleTimeString().slice(0, 5));
-                $('.cella').eq(8).find('span').text((new Date(risposta.sys.sunset * 1000)).toLocaleTimeString().slice(0, 5));
+                $('.cella').eq(2).find('span').text(risposta.wind.speed);
+                $('.cella').eq(3).find('span').text(risposta.main.pressure);
+                $('.cella').eq(4).find('span').text(risposta.main.humidity);
+                $('.cella').eq(5).find('span').text((new Date(risposta.sys.sunrise * 1000)).toLocaleTimeString().slice(0, 5));
+                $('.cella').eq(6).find('span').text((new Date(risposta.sys.sunset * 1000)).toLocaleTimeString().slice(0, 5));
                 $('.flexCol').find('img').attr('src', 'icone/' + risposta.weather[0].icon + '.svg');
                 $('main').find('h2').text(risposta.name + ', ' + risposta.sys.country);
                 impostaSfondo(risposta.weather[0].icon);
@@ -48,8 +47,8 @@ $(document).ready(function() {
             .always(function() {
                 window.alert('conclusa');
             });
-        /*});*/
-    }
+    });
+    /* }*/
 
     function impostaSfondo(tempo) {
         /*window.alert('Funzione sfondo chiamata da: ' + tempo);*/
@@ -91,7 +90,7 @@ $(document).ready(function() {
                 window.alert('Nessuno sfondo selezionato');
                 break;
         }
-        $('html').css('backgroundImage', 'url(immagini/' + imgSfondo + '.jpg)');
+        $('body').css('backgroundImage', 'url(immagini/' + imgSfondo + '.jpg)');
     }
 
 });
